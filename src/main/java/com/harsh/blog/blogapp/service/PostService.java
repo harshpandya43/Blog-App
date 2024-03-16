c682b193d4a782efd52b3086f05016efcb68b717
harshpandya43 <pandyaharsh43@gmail.com>
2024-03-16 14:32:58 -0500

    Enable order in sorting and added constants for default values

--- src/main/java/com/harsh/blog/blogapp/service/PostService.java
+++ src/main/java/com/harsh/blog/blogapp/service/PostService.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface PostService {
     PostDTO createPost(PostDTO postDTO);
 
-    PostResponse getAllPosts(int pageNo, int pageSize, String sortBy);
+    PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
 
     PostDTO getPostById(long id);
 
